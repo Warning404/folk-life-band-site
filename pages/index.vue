373b8f84200b833ff73992d284f4f7bd3523fdf0
Mai-Keed <mai.rok101@gmail.com>
2025-07-10 01:08:50 +0700

    modified:   pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -54,11 +54,11 @@
       </div>
       
       <!-- Scroll Down Indicator -->
-      <div class="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <a href="#music" class="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
         </svg>
-      </div>
+      </a>
     </section>
 
     <!-- Music Player Section -->
